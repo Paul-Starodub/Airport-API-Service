9e37fbcad307c18b5ae062dff949da69962d8912
Paul Starodub <amorallex@gmail.com>
2023-10-18 17:36:58 +0300

    Updated airplanes/models.py

--- airplanes/models.py
+++ airplanes/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class AirplaneType(models.Model):
+    """Type of airplane"""
+
+    name = models.CharField(max_length=63, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Airplane(models.Model):
+    """Airplane characteristics"""
+
+    name = models.CharField(max_length=63)
+    rows = models.PositiveIntegerField()
+    seats_in_row = models.PositiveIntegerField()
+    airplane_type = models.ForeignKey(
+        to=AirplaneType, on_delete=models.CASCADE, related_name="airplanes"
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ("name",)

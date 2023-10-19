0c83c2cb5474fedc989531d0764107b04a08d162
Paul Starodub <amorallex@gmail.com>
2023-10-19 11:45:06 +0300

    Added locations/serializers.py

---
+++ locations/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+
+from locations.models import Route, Airport
+
+
+class AirportSerializer(serializers.Serializer):
+    class Meta:
+        model = Airport
+        fields = ("id", "name", "closest_big_city")
+
+
+class RouteDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Route
+        fields = ("source", "destination", "distance")
+
+
+class RouteListSerializer(RouteDetailSerializer):
+    source = AirportSerializer(many=False, read_only=True)
+    destination = AirportSerializer(many=False, read_only=True)

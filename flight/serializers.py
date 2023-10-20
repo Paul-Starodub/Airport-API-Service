8a26c0267c0f65bafe74c5d89e054449afa93539
Paul Starodub <amorallex@gmail.com>
2023-10-20 23:00:15 +0300

    Added flight/serializers.py

---
+++ flight/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+
+from airplanes.serializers import (
+    AirplaneListSerializer,
+    AirplaneDetailSerializer,
+)
+from flight.models import Crew, Flight
+from locations.serializers import RouteListSerializer, RouteDetailSerializer
+
+
+class CrewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Crew
+        fields = ("id", "first_name", "last_name")
+
+
+class FlightSerializer(serializers.ModelSerializer):
+    def validate_crews(self, crews: list[Crew]) -> list[Crew]:
+        if len(crews) != len(set(crews)):
+            raise serializers.ValidationError(
+                "A crew member cannot be added more than once to a flight."
+            )
+        return crews
+
+    class Meta:
+        model = Flight
+        fields = (
+            "id",
+            "route",
+            "airplane",
+            "departure_time",
+            "arrival_time",
+            "crews",
+        )
+
+
+class FlightListSerializer(FlightSerializer):
+    airplane = AirplaneListSerializer(many=False, read_only=True)
+    route = RouteListSerializer(many=False, read_only=True)
+    crews = serializers.StringRelatedField(many=True, read_only=True)
+
+
+class FlightDetailSerializer(FlightSerializer):
+    airplane = AirplaneDetailSerializer(many=False, read_only=True)
+    route = RouteDetailSerializer(many=False, read_only=True)
+    crews = CrewSerializer(many=True, read_only=True)

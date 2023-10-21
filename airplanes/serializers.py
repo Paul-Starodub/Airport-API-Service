9c3b4bf0e8cea29b3c25c551cee52566eeb0a47e
Paul Starodub <amorallex@gmail.com>
2023-10-21 22:10:29 +0300

    Updated airplanes/serializers.py with property

--- airplanes/serializers.py
+++ airplanes/serializers.py
@@ -18,6 +18,17 @@ class AirplaneSerializer(serializers.ModelSerializer):
 class AirplaneDetailSerializer(AirplaneSerializer):
     airplane_type = AirplaneTypeSerializer(many=False, read_only=True)
 
+    class Meta:
+        model = Airplane
+        fields = (
+            "id",
+            "name",
+            "rows",
+            "seats_in_row",
+            "airplane_type",
+            "capacity",
+        )
+
 
 class AirplaneListSerializer(AirplaneSerializer):
     airplane_type = serializers.SlugRelatedField(

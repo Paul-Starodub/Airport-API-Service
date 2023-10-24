3d290f2554b6702acaebff2234b0bc4b595a0e75
Paul Starodub <amorallex@gmail.com>
2023-10-24 08:26:46 +0300

    Updated orders/views.py

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from typing import Type
 
-# Create your views here.
+from django.db.models import QuerySet
+
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.serializers import Serializer
+from rest_framework import mixins
+from rest_framework.permissions import IsAuthenticated
+
+from orders.models import Order
+from orders.serializers import OrderSerializer, OrderListSerializer
+
+
+class OrderViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    GenericViewSet,
+):
+    queryset = Order.objects.prefetch_related(
+        "tickets__flight__route",
+        "tickets__flight__airplane",
+        "tickets__flight__crews",
+    )
+
+    serializer_class = OrderSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self) -> QuerySet[Order]:
+        return self.queryset.filter(user=self.request.user)
+
+    def get_serializer_class(self) -> Type[Serializer]:
+        if self.action == "list":
+            return OrderListSerializer
+
+        return super().get_serializer_class()
+
+    def perform_create(self, serializer: Serializer) -> None:
+        serializer.save(user=self.request.user)

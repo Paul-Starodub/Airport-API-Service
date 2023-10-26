ec152a944940b57eb378535549a873334245ace8
Paul Starodub <amorallex@gmail.com>
2023-10-26 17:07:51 +0300

    Updated airplanes/views.py with custom endpoint

--- airplanes/views.py
+++ airplanes/views.py
@@ -1,14 +1,20 @@
-from typing import Type
+from typing import Type, Optional
 
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.request import Request
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.serializers import Serializer
 from airplanes.models import Airplane, AirplaneType
+from rest_framework.permissions import IsAdminUser
 from airplanes.permissions import IsAdminOrIfAuthenticatedReadOnly
 from airplanes.serializers import (
     AirplaneDetailSerializer,
     AirplaneTypeSerializer,
     AirplaneListSerializer,
     AirplaneSerializer,
+    AirplaneImageSerializer,
 )
 
 
@@ -34,4 +40,24 @@ class AirplaneViewSet(ModelViewSet):
         if self.action == "retrieve":
             return AirplaneDetailSerializer
 
+        if self.action == "upload_image":
+            return AirplaneImageSerializer
+
         return super().get_serializer_class()
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="upload-image",
+        permission_classes=[IsAdminUser],
+    )
+    def upload_image(
+        self, request: Request, pk: Optional[int] = None
+    ) -> Response:
+        """Endpoint to uploading image to specific bus"""
+
+        bus = self.get_object()
+        serializer = self.get_serializer(bus, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)

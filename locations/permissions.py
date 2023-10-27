1e79ea64d6f47b56b8976cf2e946267465c6d9b8
Paul Starodub <amorallex@gmail.com>
2023-10-27 09:03:21 +0300

    Added locations/permissions.py, updated views.py

---
+++ locations/permissions.py
@@ -0,0 +1,22 @@
+from django.views import View
+from rest_framework.permissions import SAFE_METHODS, BasePermission
+from rest_framework.request import Request
+
+
+class IsAuthenticatedOrAnonymous(BasePermission):
+    """Permissions for authorized/unauthorized users"""
+
+    def has_permission(self, request: Request, view: View) -> bool:
+        return bool(
+            (
+                request.method in SAFE_METHODS
+                and request.user
+                and request.user.is_authenticated
+            )
+            or (request.user and request.user.is_staff)
+        ) or (
+            request.user
+            and request.user.is_authenticated
+            and request.method == "POST"
+            and view.action in ("evaluate",)
+        )

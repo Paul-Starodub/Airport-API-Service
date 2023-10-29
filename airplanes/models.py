3dceb7d9e3683492c6794885f52f61957759761e
Paul Starodub <amorallex@gmail.com>
2023-10-29 19:35:35 +0200

    Fixed airplanes/models.py

--- airplanes/models.py
+++ airplanes/models.py
@@ -18,7 +18,7 @@ class AirplaneType(models.Model):
 def plane_image_file_path(instance: Airplane, filename: str) -> str:
     _, extension = os.path.splitext(filename)
     filename = f"{slugify(instance.name)}-{uuid.uuid4()}{extension}"
-    return os.path.join("uploads/buses/", filename)
+    return os.path.join("uploads/planes/", filename)
 
 
 class Airplane(models.Model):
